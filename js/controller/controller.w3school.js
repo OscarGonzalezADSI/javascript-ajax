94dd13ace8447e8600dabbc0f41f5560f918acec
OSCAR GONZALEZ <55475821+OscarGonzalezADSI@users.noreply.github.com>
2022-07-13 14:52:18 -0500

    Create controller.w3school.js

---
+++ js/controller/controller.w3school.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    document.getElementById("enviar").addEventListener("click", ()=>{
+        const data = 'fname=Henry&lname=Ford';
+        new RoutesW3school('/hello', 'POST', data);
+        document.getElementById("menu").click();
+    });
+    
+    document.getElementById("enviar2").addEventListener("click", ()=>{
+        new RoutesW3school('/loadDoc', 'GET', '');
+        document.getElementById("menu").click();
+    });
+    
+    document.getElementById("enviar3").addEventListener("click", ()=>{
+        new RoutesW3school('/demo-get', 'GET', '');
+        document.getElementById("menu").click();
+    });
+    
+    document.getElementById("enviar4").addEventListener("click", ()=>{
+        const data = Math.random();
+        new RoutesW3school('/demo-get/', 'GET', data);
+        document.getElementById("menu").click();
+    });
+    
+    document.getElementById("enviar5").addEventListener("click", ()=>{
+        const data = 'fname=Henry&lname=Ford';
+        new RoutesW3school('/demo-get2/', 'GET', data);
+        document.getElementById("menu").click();
+    });
+    
+});
